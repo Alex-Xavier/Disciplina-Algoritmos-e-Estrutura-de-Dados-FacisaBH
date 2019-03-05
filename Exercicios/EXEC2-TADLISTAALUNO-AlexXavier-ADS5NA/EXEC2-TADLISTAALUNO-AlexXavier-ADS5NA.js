45acdea906cbc2fc12773c8268ab17225784da09
Alex Xavier <ajxv20@gmail.com>
2019-03-05 19:15:23 -0300

    Evolução da operação mostraElementoLista para mostraAlunoLista

--- Exercicios/EXEC2-TADLISTAALUNO-AlexXavier-ADS5NA/EXEC2-TADLISTAALUNO-AlexXavier-ADS5NA.js
+++ Exercicios/EXEC2-TADLISTAALUNO-AlexXavier-ADS5NA/EXEC2-TADLISTAALUNO-AlexXavier-ADS5NA.js
@@ -8,13 +8,13 @@ function criaListaAlunos () {
 
 //Operação inserir-na-lista-aluno
 function inseriAlunoLista (aluno) {
-    listaNumeros.push(aluno);
+    listaAlunos.push(aluno);
     return console.log("Aluno",aluno,"inserido!");
 }
 
 //Operação eliminar-aluno-da-lista
 function deletaAlunoLista (indice) {
-    listaNumeros.splice(pos, indice);
+    listaAlunos.splice(pos, indice);
     return console.log("Aluno no indice ",indice," deletado da lista!");
 }
 
@@ -33,7 +33,7 @@ function quantidadeListaAluno () {
 
 //Operação aluno-esta-na-lista
 function procurarMatriculaAlunoLista (matricula) {
-    for (i=0; i<listaNumeros.length; i++) {
+    for (i=0; i<listaAlunos.length; i++) {
         if (matricula == listaAlunos[i].matricula) {
             return console.log("Aluno com a matrícula ",matricula," está na lista na posição ",i,"!");
         }
@@ -41,16 +41,15 @@ function procurarMatriculaAlunoLista (matricula) {
     return console.log("Aluno com a matrícula ",matricula," não foi encontrado!");
 }
 
-//Operação imprimir
-function mostraElementosLista () {
-    i=0;
-    do{
-        if (listaNumeros[i] == null) {
-            console.log("Vazio");
-        }else{
-            console.log(listaNumeros[i]);
-        }
-        i++;
-    }while(i<listaNumeros.length);
-    return 0;
+//Operação imprimir-aluno
+function mostraAlunoLista () {
+    if (listaAlunos[0] == null) {
+        return console.log("A lista de alunos está vazia!");
+    }
+    for (i=0; i<listaAlunos.length; i++) {
+        console.log("Nome: ",listaAlunos[i].nome);
+        console.log("Matrícula: ",listaAlunos[i].matricula);
+        console.log("Soma de Nostas: ",listaAlunos[i].somatorioNotas);
+    }
+    return console.log("Todos os alunos foram mostrados!");
 }
