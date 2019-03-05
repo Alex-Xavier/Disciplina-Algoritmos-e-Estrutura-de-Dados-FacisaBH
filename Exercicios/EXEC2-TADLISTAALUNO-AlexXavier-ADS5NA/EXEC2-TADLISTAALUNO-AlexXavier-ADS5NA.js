338ea9b448a626172049d30d8b6c1c49410f8381
Alex Xavier <ajxv20@gmail.com>
2019-03-05 19:35:37 -0300

    Mudança do nome da lista e melhora da operação deletaAlunoLista

--- Exercicios/EXEC2-TADLISTAALUNO-AlexXavier-ADS5NA/EXEC2-TADLISTAALUNO-AlexXavier-ADS5NA.js
+++ Exercicios/EXEC2-TADLISTAALUNO-AlexXavier-ADS5NA/EXEC2-TADLISTAALUNO-AlexXavier-ADS5NA.js
@@ -1,40 +1,49 @@
 //Exercício 2 -> TAD lista-aluno
 
-//Operação criar-lista-alunos
-function criaListaAlunos () {
+//Operação criar-lista-aluno
+function criaListaAluno () {
     console.log("Lista de alunos criada!");
-    return listaAlunos = [];
+    return listaAluno = [];
 }
 
 //Operação inserir-na-lista-aluno
 function inseriAlunoLista (aluno) {
-    listaAlunos.push(aluno);
+    listaAluno.push(aluno);
     return console.log("Aluno",aluno,"inserido!");
 }
 
 //Operação eliminar-aluno-da-lista
-function deletaAlunoLista (indice) {
-    listaAlunos.splice(pos, indice);
-    return console.log("Aluno no indice ",indice," deletado da lista!");
+//Passe como parâmetro a matrícula do aluno que deseja deletar
+//Se o aluno for deletado será retornado sua matrícula, caso não seja encontrado será retornado uma mensagem
+function deletaAlunoLista (matricula) {
+    for (i=0; i<listaAluno.length; i++) {
+        if (listaAluno[i].matricula == matricula) {
+            console.log("Aluno ",listaAluno[i].nome," matricula",listaAluno[i].matricula," foi deletado!");
+            listaAluno[i] = null;
+            return matricula;
+        }
+    }
+    return console.log("Aluno com a matrícula ",listaAluno[i].matricula," não foi encontrado!");
 }
 
 //Operação finalizar-lista-aluno
 function destroiListaAluno () {
-    for (i=0; i<listaAlunos.length; i++) {
-        listaAlunos[i] = null;
+    for (i=0; i<listaAluno.length; i++) {
+        listaAluno[i] = null;
     }
     return console.log("A lista de alunos foi deletada e está totalmente vazia!");
 }
 
 //Operação tamanho-lista-aluno
 function quantidadeListaAluno () {
-    return console.log("A lista contém ",listaAlunos.length," alunos!");
+    return console.log("A lista contém ",listaAluno.length," alunos!");
 }
 
 //Operação aluno-esta-na-lista
+//Passe como parâmetro a matrícula do aluno
 function procurarMatriculaAlunoLista (matricula) {
-    for (i=0; i<listaAlunos.length; i++) {
-        if (matricula == listaAlunos[i].matricula) {
+    for (i=0; i<listaAluno.length; i++) {
+        if (matricula == listaAluno[i].matricula) {
             return console.log("Aluno com a matrícula ",matricula," está na lista na posição ",i,"!");
         }
     }
@@ -43,13 +52,13 @@ function procurarMatriculaAlunoLista (matricula) {
 
 //Operação imprimir-aluno
 function mostraAlunoLista () {
-    if (listaAlunos[0] == null) {
+    if (listaAluno[0] == null) {
         return console.log("A lista de alunos está vazia!");
     }
-    for (i=0; i<listaAlunos.length; i++) {
-        console.log("Nome: ",listaAlunos[i].nome);
-        console.log("Matrícula: ",listaAlunos[i].matricula);
-        console.log("Soma de Nostas: ",listaAlunos[i].somatorioNotas);
+    for (i=0; i<listaAluno.length; i++) {
+        console.log("Nome: ",listaAluno[i].nome);
+        console.log("Matrícula: ",listaAluno[i].matricula);
+        console.log("Soma de Nostas: ",listaAluno[i].somatorioNota);
     }
     return console.log("Todos os alunos foram mostrados!");
 }
